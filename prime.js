443bd911488f52a3a2e62bab3bfdfcb31e4c70cb
kishor <peddintikishor3@gmail.com>
2022-11-02 12:43:56 +0530

    prime problem

---
+++ prime.js
@@ -0,0 +1,26 @@
+function prime(number){
+
+let count=0;
+
+for(let i=0;i<=number;i++){
+if(number%i==0){
+count++;
+}
+}
+
+if(count<2){
+return true;
+}
+else{
+return false;
+}
+
+}
+
+let ans=prime(13);
+if(ans==true){
+console.log("prime number");
+}
+else{
+console.log("not  a prime");
+}
