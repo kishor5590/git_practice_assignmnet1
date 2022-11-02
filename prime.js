6f72d453322a17bcdfb75297fded369d9279af50
Deepak Yadav <25yadavdeepak@gmail.com>
2022-11-02 13:44:41 +0530

    I have done towards my side Deepak

--- prime.js
+++ prime.js
@@ -2,13 +2,13 @@ function prime(number){
 
 let count=0;
 
-for(let i=0;i<=number;i++){
+for(let i=1;i<=number;i++){
 if(number%i==0){
 count++;
 }
 }
 
-if(count<2){
+if(count===2){
 return true;
 }
 else{
